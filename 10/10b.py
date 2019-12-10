9e6ee0360fa85b58b9c8a2bf99ed438b9f0f1f98
Slaff <slafb@amazon.com>
2019-12-10 18:44:11 -0500

    10b works now

--- 10/10b.py
+++ 10/10b.py
@@ -6,7 +6,10 @@ def get_args():
 
 	parser = argparse.ArgumentParser()
 	parser.add_argument("infile", type=str, help="Path to the input file")
-
+	parser.add_argument("x", type=int, help="x coord of center")
+	parser.add_argument("y", type=int, help="y coord of center")
+	parser.add_argument("n", type=int, help="nth asteroid to be destroyed")
+	
 	return parser.parse_args()
 
 def get_asteroid_coords(args):
@@ -35,8 +38,6 @@ def get_asteroid_coords(args):
 
 	return coords_arr.astype(int)
 
-
-
 def get_ratios(pair):
 
 	x=pair[0]
@@ -50,8 +51,8 @@ def get_ratios(pair):
 
 	elif x==0:
 		if y>0:
-			return np.asarray([0,1])
-		return np.asarray([0,-1])
+			return np.asarray([0,-1])
+		return np.asarray([0,1])
 
 	if x>0:
 		return np.asarray([1,0])
@@ -60,8 +61,8 @@ def get_ratios(pair):
 
 def get_theta(pair):
 
-	x=pair[0]
-	y=pair[1]
+	x= pair[0]
+	y= -pair[1] # in coordinates, y going down increases. in my theta space, y going up increases.
 
 	hyp= (x**2 + y**2)**0.5 # hypotnuse length
 
@@ -73,10 +74,10 @@ def get_theta(pair):
 		return 0 # hopefully never
 
 	if x>0:
-		return 90+( 180-np.degrees(np.arccos(y/hyp)) )
+		return 90-np.degrees(np.arcsin(y/hyp))
 
 	#x<0
-	return 180+np.degrees( np.arccos(y/hyp) )
+	return 270+np.degrees( np.arcsin(y/hyp) )
 
 
 
@@ -89,7 +90,8 @@ def run_main():
 	max_idx = None
 
 	# targ = np.asarray([29, 28])
-	targ = np.asarray([11, 13])
+	# targ = np.asarray([11, 13])
+	targ = np.asarray([args.x, args.y])
 
 	cent_coords = coords_arr - targ
 
@@ -106,13 +108,14 @@ def run_main():
 	final[:, 5]=dsq
 	final[:,6]=[-1]*final.shape[0] # will contain final order
 
+	final=final[ final[:,5] != 0 ] # remove the base asteroid, we don't shoot our own asteroid
+
 	final=final[final[:,4].argsort()]
 	# now columns 0-1 are original coords, 2-3 are reduced ratio coords, 4 is theta (clockwise from top), 5 is sq distance from target, sorted by THETA
 	# now will pass through and set destroy order for one ast at a time per theta, in order of increasing theta (theta=0 should be 12 oclock, not 3 oclock)
 
 	order_idx = 1 # count from ONE not ZERO so that 200th is right
 	last_added_theta = -1
-	last_added_j = -1
 
 	while np.min( final[:,6] ) < 1:
 
@@ -135,13 +138,16 @@ def run_main():
 
 	final=final[final[:,6].argsort()]
 
-	print("FINAL")
-	print(final[199,6])
-	print(final[199,4])
-	print(final[199,:])
+	final_x = final[args.n-1,0]
+	final_y = final[args.n-1,1]
+	print(f"Key: {int(100*final_x+final_y)}")
 
 	return 0
 
+def debug(s):
+
+	print(s)
+	return None
 
 if __name__ == '__main__':
 
